23a08df993848a9dc7c3d2f7ba4f1e01e3e1f8e3
nadillaxoloves81 <nadila.syihaq8@gmail.com>
2020-01-21 15:27:39 +0700

    tcl12 amiin

--- resources/views/anggaran/rekap.blade.php
+++ resources/views/anggaran/rekap.blade.php
@@ -5,6 +5,7 @@
     .border{
         border: 1px solid black;
         padding-left: 2%;
+        padding-right: 2%;
         padding-top: 1%;
         padding-bottom: 1%
     }
@@ -13,6 +14,9 @@
         border-collapse: collapse;
         font-size: 12px;
         padding: 4px
+    }
+    tfoot td{
+        border: none !important;
     }
 </style>
 <div class="main">
@@ -26,7 +30,7 @@
             <div class="border">
                 <table width="100%">
                     <tr>
-                        <td>Urusan Pemerintahan</td>
+                        <td width="20%">Urusan Pemerintahan</td>
                         <td>:</td>
                         <td>1.02.10 - Komunikasi dan Informatika</td>
                     </tr>
@@ -107,10 +111,114 @@
 
             </div>
 
-            <div class="border">
+            <div style="border: 1px solid black; padding:0;">
 
-                <table class="tabel-border" width="100%">
-                    
+                <table class="tabel-border text-center" width="100%">
+                    <tr>
+                        <td rowspan="3">Kode Rekening</td>
+                        <td rowspan="3">Uraian</td>
+                        <td colspan="4">Sebelum Perubahan</td>
+                        <td colspan="4">Setelah Perubahan</td>
+                        <td colspan="2">Bertambah/Berkurang</td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="3">Rincian Perhitungan</td>
+                        <td rowspan="2">Jumlah (Rp)</td>
+                        <td colspan="3">Rincian Perhitungan</td>
+                        <td rowspan="2">Jumlah (Rp)</td>
+                        <td rowspan="2">Rp</td>
+                        <td rowspan="2">%</td>
+                    </tr>
+
+                    <tr>
+                        <td>Volume</td>
+                        <td>Satuan</td>
+                        <td>Harga Satuan</td>
+                        <td>Volume</td>
+                        <td>Satuan</td>
+                        <td>Harga Satuan</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6 = ( 3 x 5 )</td>
+                        <td>7</td>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                        <td>12</td>
+                    </tr>
+
+                    <tr>
+                        <td>5.2</td>
+                        <td>BELANJA LANGSUNG</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>226.061.200,00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>96.521.200,00</td>
+                        <td>(129.240.000,00)</td>
+                        <td>(57)</td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="5">Jumlah</td>
+                        <td>226.061.200,00</td>
+                        <td colspan="3"></td>
+                        <td>96.821.200,00</td>
+                        <td>(129.240.000,00)</td>
+                        <td>(57)</td>
+                    </tr>
+
+                    <tfoot>
+                        <tr>
+                            <td colspan="9"></td>
+                            <td colspan="2">Padang</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="9"></td>
+                            <td colspan="2"><b>Pengguna Anggaran</b></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="9"></td>
+                            <td colspan="2" style="height: 50px"></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="9"></td>
+                            <td colspan="2"><b><u>Suardi</u></b></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="9"></td>
+                            <td colspan="2">NIP</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="12" class="text-left" style="padding-left: 10px">Keterangan </td>
+                        </tr>
+                        <tr>
+                            <td colspan="12" class="text-left" style="padding-left: 10px">Tanggal Pembahasan</td>
+                        </tr>
+                        <tr>
+                            <td colspan="12" class="text-left" style="padding-left: 10px">Catatan Hasil Pembahasan</td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="12" style="height:30px"></td>
+                        </tr>
+                    </tfoot>
                 </table>
 
             </div>
